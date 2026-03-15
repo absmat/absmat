a9a3c4d55644dadd7bef646e53d1c8b50e2437db
absmat <absmatservice@outlook.com>
2026-03-15 18:02:37 +0200

    Ensimmäinen versio
    Perustiedostot lisätty

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'absmat-cache-v9';
+
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png',
+  'https://fonts.googleapis.com/css2?family=Open+Sans:wght@300..800&display=swap'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
